fc646c2dddb1acf3d3e26998910323b635b54db5
sosky-yty <1027797811@qq.com>
2018-07-09 01:47:21 +0800

    页面控制成功

---
+++ src/main/java/com/skypointer/huaji/controller/page/IndexController.java
@@ -0,0 +1,34 @@
+package com.skypointer.huaji.controller.page;
+
+import com.skypointer.huaji.bean.User;
+import com.skypointer.huaji.controller.BaseContrller;
+import com.skypointer.huaji.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class IndexController extends BaseContrller {
+    @Autowired
+    private IUserService userService;
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value={"/index"})
+    public String index(){
+        List<User> users = userService.findAll();
+        logger.debug(users.toString());
+        return "index";
+    }
+
+    @RequestMapping(value={"/"})
+    public String login(){
+        List<User> users = userService.findAll();
+        logger.debug(users.toString());
+        return "login";
+    }
+}
---
+++ src/main/resources/templates/login.ftl
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>角色列表</title>
+    <meta name="keywords" content="图书馆管理">
+    <meta name="description" content="Spring Boot开发项目">
+</head>
+
+<body>
+<h1> 控制成功!!!</h1>
+</body>
+
+</html>
